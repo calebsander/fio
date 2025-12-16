8ff0415583bdf3b7dee27fb6d8643a039a9a5e27
Caleb Sander Mateos <csander@purestorage.com>
2025-12-16 10:21:07 -0700

    io_uring: remove unnecessary branch in fio_ioring_event()
    There is already an early return if cqe->res == io_u->xfer_buflen. So
    the condition cqe->res != io_u->xfer_buflen will always be true. Drop
    the unnecessary condition.
    
    Signed-off-by: Caleb Sander Mateos <csander@purestorage.com>

--- engines/io_uring.c
+++ engines/io_uring.c
@@ -693,18 +693,14 @@ static struct io_u *fio_ioring_event(struct thread_data *td, int event)
 		return io_u;
 	}
 
-	if (cqe->res != io_u->xfer_buflen) {
-		if (io_u->ddir == DDIR_TRIM) {
-			ld->async_trim_fail = 1;
-			cqe->res = 0;
-		}
-		if (cqe->res > io_u->xfer_buflen)
-			io_u->error = -cqe->res;
-		else
-			io_u->resid = io_u->xfer_buflen - cqe->res;
-
-		return io_u;
+	if (io_u->ddir == DDIR_TRIM) {
+		ld->async_trim_fail = 1;
+		cqe->res = 0;
 	}
+	if (cqe->res > io_u->xfer_buflen)
+		io_u->error = -cqe->res;
+	else
+		io_u->resid = io_u->xfer_buflen - cqe->res;
 
 	return io_u;
 }
